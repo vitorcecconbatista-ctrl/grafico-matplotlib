1dbbf5518232868cf1787483e88730ddbf183e4a
vitorcecconbatista-ctrl <vitor.ceccon.batista@escola.pr.gov.br>
2026-03-20 15:09:40 +0000

    criação grafico

--- grafico.py
+++ grafico.py
@@ -1,1 +1,9 @@
 
+import matplotlib.pyplot as plt
+meses = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho']
+vendas = [200, 300, 340, 305, 300, 400]
+plt.plot(meses, vendas)
+plt.title('Vendas no Primeiro Semestre')
+plt.xlabel('Meses')
+plt.ylabel('Vendas')
+plt.savefig("grafico_linhas.png")
